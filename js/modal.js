3421caa491d53456549829c73681fc235462cf79
Mallea Paulline <info@PaullineAbrilMallea.com>
2018-03-10 20:32:53 -0500

    Updating and debugging files

--- js/modal.js
+++ js/modal.js
@@ -22,3 +22,14 @@ window.onclick = function(event) {
 modal.style.display = "none";
 }
 }
+
+
+// var $body = $(document.body);
+// var oldWidth = $body.innerWidth();
+// $body.css("overflow", "hidden");
+// $body.width(oldWidth);
+
+// var $body = $(document.body);
+// $body.css("overflow", "auto");
+// $body.width("auto");
+
