5d8a42143d3e91f401321015ce79ad674172cf14
Mallea Paulline <info@PaullineAbrilMallea.com>
2018-03-18 18:21:39 -0400

    Fourth commit: updating modals

--- js/modal1.js
+++ js/modal1.js
@@ -1,15 +1,15 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementById('myModal1');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("myBtn1");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close1")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";
+  modal.style.display = "block";z
                   }
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -22,7 +22,3 @@ window.onclick = function(event) {
 modal.style.display = "none";
 }
 }
-
-
-
-
--- js/untitled.js
+++ js/untitled.js
@@ -1,11 +1,11 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementById('myModal2');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("myBtn2");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close2")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
@@ -22,7 +22,3 @@ window.onclick = function(event) {
 modal.style.display = "none";
 }
 }
-
-
-
-
