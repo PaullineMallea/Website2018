2bf07d3db3c075ee26c32c8acd5c6da6560e30ce
Mallea Paulline <info@PaullineAbrilMallea.com>
2018-03-18 23:52:32 -0400

    Fifth commit: updating modals

--- js/modal1.js
+++ js/modal1.js
@@ -1,5 +1,5 @@
 // Get the modal
-var modal = document.getElementById('myModal1');
+var modal1 = document.getElementById('myModal1');
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn1");
@@ -9,16 +9,16 @@ var span = document.getElementsByClassName("close1")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";z
+  modal1.style.display = "block";z
                   }
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modal1.style.display = "none";
                   }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-	if (event.target == modal) {
-modal.style.display = "none";
+	if (event.target == modal1) {
+modal1.style.display = "none";
 }
 }
