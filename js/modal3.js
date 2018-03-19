2e1ed729e319c8c6fd57b394943f9a65d8a85a07
Mallea Paulline <info@PaullineAbrilMallea.com>
2018-03-19 04:49:56 -0400

    Sixth commit: Finished adding View Buttons on timeline

--- js/modal3.js
+++ js/modal3.js
@@ -1,24 +1,24 @@
 // Get the modal
-var modal4 = document.getElementById('myModal4');
+var modal3 = document.getElementById('myModal3');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn4");
+var btn = document.getElementById("myBtn3");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close4")[0];
+var span = document.getElementsByClassName("close3")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal4.style.display = "block";z
+  modal3.style.display = "block";z
                   }
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal4.style.display = "none";
+  modal3.style.display = "none";
                   }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-	if (event.target == modal4) {
-modal4.style.display = "none";
+	if (event.target == modal3) {
+modal3.style.display = "none";
 }
 }
--- js/modal5.js
+++ js/modal5.js
@@ -1,24 +1,24 @@
 // Get the modal
-var modal4 = document.getElementById('myModal4');
+var modal5 = document.getElementById('myModal5');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn4");
+var btn = document.getElementById("myBtn5");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close4")[0];
+var span = document.getElementsByClassName("close5")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal4.style.display = "block";z
+  modal5.style.display = "block";z
                   }
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal4.style.display = "none";
+  modal5.style.display = "none";
                   }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-	if (event.target == modal4) {
-modal4.style.display = "none";
+	if (event.target == modal5) {
+modal5.style.display = "none";
 }
 }
